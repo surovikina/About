536432dabb708da7fe3bea1543a508eb9f4d7d0c
surovikina <korol0955559531@gmail.com>
2017-09-26 18:04:18 +0300

    Added website sources

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp        = require('gulp'),
+    less        = require('gulp-less'),
+    browserSync = require('browser-sync');
+
+gulp.task('less',function(){
+  return gulp.src('app/less/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server:{
+      baseDir: 'app'
+    },
+    notify: false
+  });
+});
+
+gulp.task('watch',['browser-sync', 'less'], function(){
+  gulp.watch('app/less/main.less', ['less']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
